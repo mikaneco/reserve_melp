9805a76a7a2d2e453286eee96e693e4fd86fd65b
MichiKaneko <michihito1005@gmail.com>
2023-02-20 21:54:23 +0900

    クリニックのmigrationを修正

--- db/migrate/20230220124009_create_clinics.rb
+++ db/migrate/20230220124009_create_clinics.rb
@@ -1,13 +1,13 @@
 class CreateClinics < ActiveRecord::Migration[7.0]
   def change
     create_table :clinics do |t|
-      t.string :name, null: false
-      t.string :building, null: false
-      t.string :address, null: false
-      t.string :city, null: false
-      t.string :prefecture, null: false
-      t.string :phone, null: false
-      t.string :url
+      t.string :name, null: false, default: ""
+      t.string :building, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.string :city, null: false, default: ""
+      t.string :prefecture, null: false, default: ""
+      t.string :phone, null: false, default: ""
+      t.string :url, null: false, default: ""
 
       t.timestamps
     end
